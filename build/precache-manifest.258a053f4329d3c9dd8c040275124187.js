65959c122cb8dad448d4b9b18a5787bdbb2d2baf
Rachel Ombok <rbo224@nyu.edu>
2020-08-09 23:06:59 -0400

    try again

---
+++ build/precache-manifest.258a053f4329d3c9dd8c040275124187.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370be4f38fb66202c5a088a2680ddd98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cda8b16fa7c3ef849f65",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "2ee645e3a511c9002ced",
+    "url": "/static/css/main.058d3143.chunk.css"
+  },
+  {
+    "revision": "cda8b16fa7c3ef849f65",
+    "url": "/static/js/2.74511833.chunk.js"
+  },
+  {
+    "revision": "1e0e3e4f079e879b1b5d8c39b0df90d3",
+    "url": "/static/js/2.74511833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee645e3a511c9002ced",
+    "url": "/static/js/main.67152dd5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e6ddd865befaad84a878764f7140d868",
+    "url": "/static/media/COVIDforecasts-logo-white.e6ddd865.png"
+  }
+]);
--- old/static/javascript/signin.js
+++
@@ -1,8 +0,0 @@
-
-console.log(document.querySelector('.img-btn'));
-document.querySelector('.img-btn').addEventListener('click', function () {
-    document.querySelector('.cont').classList.toggle('s-signup')
-}
-);
-
-console.log("done")
