a126da5982d8064df193eab0fcd85a8f905c9813
Rachel Ombok <rbo224@nyu.edu>
2020-08-12 10:26:16 -0400

    retry

--- src/components/Navbar/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Navbar from './';
-import renderer from 'react-test-renderer';
-
-test('Navbar renders correctly', () => {
-  const component = renderer.create(<Navbar />);
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
